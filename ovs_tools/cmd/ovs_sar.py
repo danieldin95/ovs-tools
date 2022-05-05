0647f10311667395802c48146278015c8d893c08
Daniel Ding <danieldin95@163.com>
2022-05-06 01:14:29 +0800

    fea: support bridge interface.

--- ovs_tools/cmd/ovs_sar.py
+++ ovs_tools/cmd/ovs_sar.py
@@ -32,11 +32,19 @@ def find_bridge(port):
     return code, output
 
 
+def is_bridge(port):
+    code, _ = ovs_vsctl('br-exists', port)
+    return code == 0
+
+
 def find_statistics(port):
     try:
         code, bridge = find_bridge(port)
         if code != 0:
-            raise Exception(bridge)
+            if is_bridge(port):
+                bridge = port
+            else:
+                raise Exception(bridge)
         code, output = ovs_ofctl('dump-ports', bridge, port)
         if code != 0:
             raise Exception(output)
@@ -46,35 +54,46 @@ def find_statistics(port):
                         output, re.S)
         return [int(i) for i in rx[0]], [int(i) for i in tx[0]]
     except Exception as e:
-        sys.stdout.write("%s\n"% e)
+        sys.stdout.write("%s\n" % e)
         return [0] * 4, [0] * 4
 
 
 def run():
-    inl = args.interval * 1.0
+    interval = args.interval * 1.0
     ports = args.interface.split(',')
-    last = time.time()
+    start_time = time.time()
     sys.stdout.write(('%-6.2f %-13s' + ' %-10s' * 8 + '\n') %
-                     (0, 'IFACE', 'rxpck/s', 'txpck/s', 'rxkB/s',
-                      'txkB/s', 'rxerr/s', 'txerr/s', 'rxdrop/s', 'txdrop/s'))
+                     (0, 'IFACE',
+                      'rxpck/s', 'txpck/s',
+                      'rxkB/s', 'txkB/s',
+                      'rxerr/s', 'txerr/s',
+                      'rxdrop/s', 'txdrop/s'))
+    rx0 = {}
+    tx0 = {}
+    for i in ports:
+        rx0[i], tx0[i] = find_statistics(i)
+    record = time.time()
     while True:
-        rx0 = {}
-        tx0 = {}
         rx1 = {}
         tx1 = {}
-        record = time.time()
-        for i in ports:
-            rx0[i], tx0[i] = find_statistics(i)
-        time.sleep(inl - (time.time() - record))
+        time.sleep(interval)
         for i in ports:
             rx1[i], tx1[i] = find_statistics(i)
+        dt = time.time() - record
         for i in ports:
             sys.stdout.write(('%-6.2f %-13s' + ' %-10.2f' * 8 + '\n') %
-                             (time.time() - last, i,
-                              (rx1[i][0] - rx0[i][0]) / inl, (tx1[i][0] - tx0[i][0]) / inl,
-                              (rx1[i][1] - rx0[i][1]) / 1024.0 / inl, (tx1[i][1] - tx0[i][1]) / 1024.0 / inl,
-                              (rx1[i][2] - rx0[i][2]) / inl, (tx1[i][2] - tx0[i][2]) / inl,
-                              (rx1[i][3] - rx0[i][3]) / inl, (tx1[i][3] - tx0[i][3]) / inl))
+                             (time.time() - start_time, i,
+                              (rx1[i][0] - rx0[i][0]) / dt,
+                              (tx1[i][0] - tx0[i][0]) / dt,
+                              (rx1[i][1] - rx0[i][1]) / 1024.0 / dt,
+                              (tx1[i][1] - tx0[i][1]) / 1024.0 / dt,
+                              (rx1[i][2] - rx0[i][2]) / dt,
+                              (tx1[i][2] - tx0[i][2]) / dt,
+                              (rx1[i][3] - rx0[i][3]) / dt,
+                              (tx1[i][3] - tx0[i][3]) / dt))
+        rx0 = rx1
+        tx0 = tx1
+        record = time.time()
 
 
 def setup():
